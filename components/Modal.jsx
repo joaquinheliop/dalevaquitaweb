54ec413e25a398531d1f8f2d94a943762489b05a
Joaquin <joaquin.helio.p@hotmail.com>
2022-04-17 17:34:04 -0300

    Modal

---
+++ components/Modal.jsx
@@ -0,0 +1,8 @@
+const Modal = () => {
+    return (
+        <div className="fixed top-0 right-0 left-0 z-50 w-screen h-screen bg-black bg-opacity-50 flex items-center justify-center">
+            <div className="w-1/3 h-1/3 bg-white shadow-lg ">asdasd</div>
+        </div>
+    )
+}
+export default Modal
